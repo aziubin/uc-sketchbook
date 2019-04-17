d47c81f481a5778766c9b3de9e25f163109eac91
arsenziubin <arsenziubin@gmail.com>
2019-04-17 14:46:06 +0300

    one and two stepper motors, standart arduino libreri

---
+++ stepper_oneRevolution2MATORA/stepper_oneRevolution2MATORA.ino
@@ -0,0 +1,48 @@
+
+/*
+ Stepper Motor Control - one revolution
+
+ This program drives a unipolar or bipolar stepper motor.
+ The motor is attached to digital pins 8 - 11 of the Arduino.
+
+ The motor should revolve one revolution in one direction, then
+ one revolution in the other direction.
+
+
+ Created 11 Mar. 2007
+ Modified 30 Nov. 2009
+ by Tom Igoe
+
+ */
+#include <Stepper.h>
+
+const int stepsPerRevolution =520;  // change this to fit the number of steps per revolution
+// for your motor
+
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper2(stepsPerRevolution, 2, 4, 3, 5);
+Stepper myStepper1(stepsPerRevolution, 9, 10, 11, 12);
+
+void setup() {
+   // set the speed at 60 rpm:
+   myStepper1.setSpeed(60);
+   myStepper2.setSpeed(60);
+
+   // initialize the serial port:
+   Serial.begin(9600);
+}
+
+void loop() {
+  
+  // step one revolution  in one direction:
+  Serial.println("clockwise");
+  myStepper1.step(stepsPerRevolution);
+    myStepper2.step(stepsPerRevolution);
+//  delay(500);
+
+  // step one revolution in the other direction:
+  Serial.println("counterclockwise");
+  myStepper1.step(-stepsPerRevolution);
+  myStepper2.step(-stepsPerRevolution);  
+//  delay(200);
+}
